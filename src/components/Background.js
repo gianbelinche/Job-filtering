ff563c141aef7548dac1f378fd6eb2ddaf82ec67
Gianbelinche <gbelinche@fi.uba.ar>
2022-06-06 14:26:00 -0300

    Add background color

--- src/components/Background.js
+++ src/components/Background.js
@@ -4,6 +4,8 @@ import background_image from "../images/bg-header-desktop.svg";
 
 export default function Background() {
   return (
-    <Image src={background_image} preview={false} style={{ width: "100%" }} />
+    <div style={{ background: "hsl(180, 29%, 50%)" }}>
+      <Image src={background_image} preview={false} style={{ width: "100%" }} />
+    </div>
   );
 }
